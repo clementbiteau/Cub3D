f5eba05f66d25ac87726e295469451ff34773353
clementbiteau <clement.biteau.mars@gmail.com>
2024-07-11 15:37:28 +0200

    axis height begin

--- srcs/raycasting/vertical_axis_height.c
+++ srcs/raycasting/vertical_axis_height.c
@@ -1,3 +1,12 @@
 #include "cub3d.h"
 
-// calculate the axis height -> renders colors on by the height
+/**
+ * Calculating the axis height is essential in our program.
+ * Keep in mind the greatest limit of DDA -> all walls are perpendicular to the floor.
+ * Meaning that unfortunately we cannot move on the "z" axis. But no matter.
+ * We still need the height of the axis or stripe being studied in comparison to
+ * the normalized window axis to successfully output the correct color or the correct pixel.
+ * 
+ * For this, we need to calculate the axis height.
+*/
+
