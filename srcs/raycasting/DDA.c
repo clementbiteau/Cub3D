8a999721678a235b779c7e8eb8d3ca91b384e14c
clementbiteau <clement.biteau.mars@gmail.com>
2024-07-11 15:20:10 +0200

    clearer dda

--- srcs/raycasting/DDA.c
+++ srcs/raycasting/DDA.c
@@ -34,31 +34,39 @@ static void set_dda(t_ray *ray, t_player *player)
     set_dda_y(ray, player);
 }
 
-static void perform_dda(t_data *data, t_ray *ray)
+static void update_ray_position(t_ray *ray)
 {
-    int wall_hit;
-
-    wall_hit = 0;
-
-    while (wall_hit == 0)
+    if (ray->dist_to_wall_x < ray->dist_to_wall_y)
     {
-        if (ray->dist_to_wall_x < ray->dist_to_wall_y)
-        {
-            ray->dist_to_wall_x += ray->dist_on_delta_x;
-            ray->map_x += ray->step_x;
-            ray->vert_or_horiz = 0;
-        }
-        else
-        {
-            ray->dist_to_wall_y += ray->dist_on_delta_y;
-            ray->map_y += ray->step_y;
-            ray->vert_or_horiz = 1;
-        }
-        if (ray->map_y < 0.25 || ray->map_x < 0.25 ||
-            ray->map_y > data->mapper.height - 0.25 ||
-            ray->map_x > data->mapper.width - 1.25)
-            break;
-        else if (data->mapper.file[ray->map_y][ray->map_x] > '0')
-            wall_hit = 1;
+        ray->dist_to_wall_x += ray->dist_on_delta_x;
+        ray->map_x += ray->step_x;
+        ray->vert_or_horiz = 0; // Vertical wall hit
+    }
+    else
+    {
+        ray->dist_to_wall_y += ray->dist_on_delta_y;
+        ray->map_y += ray->step_y;
+        ray->vert_or_horiz = 1; // Horizontal wall hit
     }
 }
+
+static int check_hit(t_data *data, t_ray *ray)
+{
+    if (ray->map_y < 0.25 || ray->map_x < 0.25 ||
+        ray->map_y > data->mapper.height - 0.25 ||
+        ray->map_x > data->mapper.width - 1.25)
+        return 1;
+    else if (data->mapper.file[ray->map_y][ray->map_x] > '0')
+        return 1;
+    return 0;
+}
+
+static void do_digital_differential_analyser(t_data *data, t_ray *ray)
+{
+    while (ray->wall_hit == 0)
+    {
+        update_ray_position(ray);
+        ray->wall_hit = check_hit(data, ray);
+    }
+}
+
