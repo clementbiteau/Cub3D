65b6c26bca029002f78d3740ce4fc776f89de18e
clementbiteau <clement.biteau.mars@gmail.com>
2024-07-15 09:37:20 +0200

    last update

--- srcs/rendering/coloring.c
+++ srcs/rendering/coloring.c
@@ -1,2 +1,20 @@
 #include "cub3d.h"
 
+static void color_by_dir(t_data *data, t_ray *ray)
+{
+    if (ray->vert_or_horiz == 0)
+    {
+        if (ray->ray_dir_x < 0)
+            data->coloring.index = WEST;
+        else
+            data->coloring.index = EAST;
+    }
+    else
+    {
+        if (ray->ray_dir_y > 0)
+            data->coloring.index = SOUTH;
+        else
+            data->coloring.index = NORTH;
+    }
+}
+
